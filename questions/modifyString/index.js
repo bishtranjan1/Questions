44005c7278d44fd8408e356758b88bb58ada6d16
Ranjan Bisht <ranbisht@publicisgroupe.net>
2019-02-18 15:35:24 +0530

    minor changes

--- questions/modifyString/index.js
+++ questions/modifyString/index.js
@@ -1,15 +1,14 @@
 // Question 1- modifyStrings  
 
-function modifyStrings(str){
-    let converter={a:4,e:3,i:1,o:0,s:5,t:7,d:5};
+function modifyString(str){
+ let converter={a:4,e:3,i:1,o:0,s:5,t:7,d:5};
+ let modifiedStr=""
 
-  for(let i=0;i<str.length;i++){
-      if(converter[str[i].toLowerCase()]){
-          str[i]=converter[str[i].toLowerCase()];
-      }
-  }
-  return str;
+for(let i=0;i<str.length;i++)
+    modifiedStr+= (converter[str[i].toLowerCase()]!=undefined)? converter[str[i].toLowerCase()]: str[i];
+  
+  return modifiedStr;
 }
 
-module.exports = modifyStrings;
+module.exports = modifyString;
 
