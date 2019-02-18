41e6616eda8bafb042c95c386e0ddd1b10e0ca95
Ranjan Bisht <ranbisht@publicisgroupe.net>
2019-02-18 15:46:07 +0530

    changes for d and b in modidy string

--- questions/modifyString/index.js
+++ questions/modifyString/index.js
@@ -1,11 +1,11 @@
 // Question 1- modifyStrings  
 
 function modifyString(str){
- let converter={a:4,e:3,i:1,o:0,s:5,t:7,d:5};
+ let converter={a:4,e:3,i:1,o:0,s:5,t:7,b:5,d:5};
  let modifiedStr=""
 
 for(let i=0;i<str.length;i++)
-    modifiedStr+= (converter[str[i].toLowerCase()]!=undefined)? converter[str[i].toLowerCase()]: str[i];
+    modifiedStr+= (str[i]!=="B" && str[i]!=="d" && converter[str[i].toLowerCase()]!=undefined)? converter[str[i].toLowerCase()]: str[i];
   
   return modifiedStr;
 }
